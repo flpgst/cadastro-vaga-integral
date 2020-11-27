efa558813e7e1b3c6afeb5f6f947aed01d34de88
Filipe Augusto Gonçalves <flpgst@gmail.com>
2020-11-27 16:55:13 -0300

    Ajustado busca de matricula por codigo

--- backend/src/app/controllers/MatriculaController.js
+++ backend/src/app/controllers/MatriculaController.js
@@ -5,13 +5,13 @@ class MatriculaController {
     if (!req.authorized)
       return res.status(401).json({ message: 'Não Autorizado' });
 
-    const { id } = req.params;
+    const { codigo } = req.params;
 
     const matricula = await Matricula.findOne({
-      where: { id },
+      where: { codigo },
       include: { all: true, nested: true },
     });
-    if (!req.superAdmin && matricula.id !== req.unidadeEnsinoId)
+    if (!req.superAdmin && matricula.unidadeEnsinoId !== req.unidadeEnsinoId)
       return res
         .status(401)
         .json({ message: 'Esta matrícula não está na sua unidade' });
