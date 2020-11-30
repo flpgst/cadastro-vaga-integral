453af61ed7faad85588d541509010bc29d6aa602
Matheus Pedroni <mpedroni77@gmail.com>
2020-11-30 15:28:58 -0300

    Mudanças no método isEmpty

--- frontend/src/models/Address.js
+++ frontend/src/models/Address.js
@@ -3,17 +3,33 @@ export default class Address {
     this.rua = null;
     this.numero = null;
     this.bairro = null;
-    this.cidade = null;
+    this.cidade = {};
     this.complemento = null;
   }
 
+  isAttrEmpty(attr) {
+    for (const key in attr) {
+      if (!attr[key]) continue;
+
+      if (!(typeof attr[key] === "object") || !this.isAttrEmpty(attr[key]))
+        return false;
+    }
+
+    return true;
+  }
+
   isEmpty() {
-    for (const attr in this) if (this[attr]) return false;
+    for (const attr in this) {
+      if (!this[attr]) continue;
+
+      if (!(typeof this[attr] === "object") && !this.isAttrEmpty(this[attr]))
+        return false;
+    }
 
     return true;
   }
 
   clear() {
-    for (const attr in this) this[attr] = null;
+    for (const attr in this) this[attr] = attr === "cidade" ? {} : null;
   }
 }
