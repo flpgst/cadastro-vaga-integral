9e4b1adcb0ddf87e977aefef6f709a09419999c8
Matheus Pedroni <mpedroni77@gmail.com>
2020-12-02 17:56:22 -0300

    Criação das view Home e ListaCadastros

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,10 +1,24 @@
-import Cadastro from "@/pages/Cadastro";
+import Home from "@/pages/Home";
+const ListaCadastros = () =>
+  import(/* webpackChunkName: "lista-cadastros" */ "@/pages/Cadastro/Lista");
+const NovoCadastro = () =>
+  import(/* webpackChunkName: "novo-cadastro" */ "@/pages/Cadastro/Novo");
 
 const routes = [
   {
     path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/cadastros",
+    name: "Cadastros",
+    component: ListaCadastros
+  },
+  {
+    path: "/cadastros/novo",
     name: "Novo Cadastro",
-    component: Cadastro
+    component: NovoCadastro
   }
 ];
 
