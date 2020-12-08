f232146a9e28f0a14a446ed5f3a0c09a14c3cb03
Matheus Pedroni <mpedroni77@gmail.com>
2020-12-08 15:58:42 -0300

    Duplicada renderização do protocolo e adicionado botão para impressão

--- frontend/src/pages/ProtocolPrint/index.vue
+++ frontend/src/pages/ProtocolPrint/index.vue
@@ -1,40 +1,76 @@
 <template>
   <v-row class="px-12" v-if="inscricao">
-    <v-col cols="12">
-      Protocolo:
-      <span class="font-weight-medium">{{ inscricao.protocolo }}</span>
-    </v-col>
+    <template v-for="protocolo in 2">
+      <v-col
+        class="pa-0 d-flex flex-wrap"
+        cols="12"
+        :key="`${protocolo}_protocolo`"
+      >
+        <v-col cols="12">
+          Protocolo:
+          <span class="font-weight-medium">{{ inscricao.protocolo }}</span>
+        </v-col>
 
-    <v-col cols="12" class="text-justify">
-      Declaro que estou de acordo com todos os dados inseridos no requerimento
-      para o período Integral. Na falta de algum documento solicitado, implicará
-      na desclassificação automática para a vaga pretendida.
-    </v-col>
+        <v-col cols="12" class="text-justify">
+          Declaro que estou de acordo com todos os dados inseridos no
+          requerimento para o período Integral. Na falta de algum documento
+          solicitado, implicará na desclassificação automática para a vaga
+          pretendida.
+        </v-col>
 
-    <v-col cols="12" class="my-5">
-      Unidade de ensino: {{ inscricao.matricula.unidadeEnsino.pessoa.nome }}
-    </v-col>
+        <v-col cols="12" class="my-5">
+          Unidade de ensino: {{ inscricao.matricula.unidadeEnsino.pessoa.nome }}
+        </v-col>
 
-    <v-col cols="6" class="my-5">
-      <v-col cols="12" class="text-center">
-        ____________________________________
-      </v-col>
-      <v-col cols="12" class="text-center">
-        Assinatura do responsável
-      </v-col>
-    </v-col>
-    <v-col cols="6" class="my-5">
-      <v-col cols="12" class="text-center">
-        ____________________________________
-      </v-col>
-      <v-col cols="12" class="text-center">
-        Assinatura do diretor/secretário
-      </v-col>
-    </v-col>
+        <v-col cols="6" class="my-5">
+          <v-col cols="12" class="text-center">
+            ____________________________________
+          </v-col>
+          <v-col cols="12" class="text-center">
+            Assinatura do responsável
+          </v-col>
+        </v-col>
+        <v-col cols="6" class="my-5">
+          <v-col cols="12" class="text-center">
+            ____________________________________
+          </v-col>
+          <v-col cols="12" class="text-center">
+            Assinatura do diretor/secretário
+          </v-col>
+        </v-col>
 
-    <v-col cols="12" class="text-end">
-      Data: {{ format(parseISO(inscricao.data_cadastro), "dd/MM/yyyy HH:mm") }}
-    </v-col>
+        <v-col cols="12" class="text-end">
+          Data:
+          {{ format(parseISO(inscricao.data_cadastro), "dd/MM/yyyy HH:mm") }}
+        </v-col>
+
+        <v-col>
+          <v-divider v-if="protocolo === 1" />
+        </v-col>
+      </v-col>
+    </template>
+    <v-fab-transition style="border: solid 1px black">
+      <v-tooltip top>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            id="cvi-print-btn"
+            v-on="on"
+            class="mr-8 mb-8"
+            @click="print"
+            color="primary"
+            fab
+            large
+            dark
+            bottom
+            fixed
+            right
+          >
+            <v-icon>mdi-printer</v-icon>
+          </v-btn>
+        </template>
+        Imprimir Protocolo
+      </v-tooltip>
+    </v-fab-transition>
   </v-row>
 </template>
 
@@ -64,13 +100,16 @@ export default {
     parseISO,
     print() {
       const appbar = document.querySelector("#app-bar");
+      const printBtn = document.querySelector("#cvi-print-btn");
 
       window.addEventListener("beforeprint", () => {
         appbar.style.display = "none";
+        printBtn.style.display = "none";
       });
 
       window.addEventListener("afterprint", () => {
         appbar.style.display = "inline";
+        printBtn.style.display = "inline";
       });
 
       window.print();
