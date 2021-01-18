b0ec614e398a70e7dd99bfbf01caf1a148f216f0
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-01-18 17:46:37 -0300

    Retornando inscricao completa

--- backend/src/app/controllers/OrdenaInscricaoController.js
+++ backend/src/app/controllers/OrdenaInscricaoController.js
@@ -1,37 +1,51 @@
 import Inscricao from '../models/Inscricao';
 import ErrorHandler from '../util/error';
 import ordernaInscricoes from '../util/order';
+import Matricula from '../models/Matricula';
+import Pessoa from '../models/Pessoa';
+import ProcessoInscricao from '../models/ProcessoInscricao';
+import UnidadeEnsino from '../models/UnidadeEnsino';
 
-class MatriculaController {
+class OrdenarInscricaoController {
   async update(req, res, next) {
     try {
       if (!req.superAdmin) {
         throw new ErrorHandler(401, 'Não autorizado');
       }
 
-      const inscricoes = await Inscricao.findAll({
-        where: {
-          ativo: true,
-        },
-      });
+      const getInscricoes = async () =>
+        await Inscricao.findAll({
+          where: {
+            ativo: true,
+          },
+          include: [
+            ProcessoInscricao,
+            {
+              model: Matricula,
+              include: [
+                { model: Pessoa, nested: true, all: true },
+                { model: UnidadeEnsino, include: Pessoa },
+              ],
+            },
+          ],
+          order: ['posicao'],
+        });
+
+      let inscricoes = await getInscricoes();
 
       let inscricoesOrdenadas = ordernaInscricoes(
         inscricoes.map(({ dataValues }) => dataValues)
       );
 
-      inscricoesOrdenadas = inscricoesOrdenadas.map((inscricao, index) => {
-        Inscricao.update(
-          { posicao: index + 1 },
-          { where: { id: inscricao.id } }
-        );
-        return { ...inscricao, posicao: index + 1 };
+      inscricoesOrdenadas.map(async ({ id }, index) => {
+        await Inscricao.update({ posicao: index + 1 }, { where: id });
       });
 
-      res.json(inscricoesOrdenadas);
+      res.json(await getInscricoes());
     } catch (error) {
       next(error);
     }
   }
 }
 
-export default new MatriculaController();
+export default new OrdenarInscricaoController();
