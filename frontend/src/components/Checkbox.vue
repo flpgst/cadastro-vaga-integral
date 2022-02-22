7b1be15d027127cb0ef243f037a9e912d8681968
Darlan Voss <darlan.voss@edu.itajai.sc.gov.br>
2022-02-22 18:31:23 -0300

    Novo Component 'Checkbox'

---
+++ frontend/src/components/Checkbox.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-checkbox
+    v-model="model"
+    :color="color"
+    :disabled="disabled"
+    :label="label"
+  ></v-checkbox>
+</template>
+
+<script>
+export default {
+  name: "CPTCheckbox",
+  props: {
+    color: {
+      type: String,
+      default: "primary"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: undefined
+    },
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    model: {
+      get() {
+        return this.value;
+      },
+      set(newValue) {
+        this.$emit("input", newValue);
+      }
+    }
+  }
+};
+</script>
